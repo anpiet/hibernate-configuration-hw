3518b926b010365fa730bd4a6329b186350e2908
Andrzej Pietkiewicz <pietan@me.com>
2025-07-30 22:22:36 +0200

    third solution

--- src/main/java/mate/academy/util/HibernateUtil.java
+++ src/main/java/mate/academy/util/HibernateUtil.java
@@ -3,11 +3,19 @@ package mate.academy.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class HibernateUtil {
+public final class HibernateUtil {
     private static final SessionFactory sessionFactory = initSessionFactory();
 
+    private HibernateUtil() {
+
+    }
+
     private static SessionFactory initSessionFactory() {
-        return new Configuration().configure().buildSessionFactory();
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to create SessionFactory", e);
+        }
     }
 
     public static SessionFactory getSessionFactory() {
